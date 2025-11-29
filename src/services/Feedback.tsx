bd579b5c6ec96d6fe2e4a80967733cc5d1c3c4aa
MartinBeccereca <martinbeccereca@gmail.com>
2025-11-29 19:44:00 -0300

    feat(feedback): Interfaz respuesta feedback

--- src/services/Feedback.tsx
+++ src/services/Feedback.tsx
@@ -1,11 +1,12 @@
 import type { Feedback } from "../feedback/interfaces/feedback-interface";
+import type { RespuestaFeedback } from "../feedback/interfaces/feedback-response";
 import api from "../utils/api";
 
 export const feedback= async (
     feedbackData: Feedback
-): Promise<FeedbackResponse> => {
+): Promise<RespuestaFeedback> => {
   try {
-    const response = await api.post<FeedbackResponse>(`/feedback`, feedbackData);
+    const response = await api.post<RespuestaFeedback>(`/feedback`, feedbackData);
     return response.data;
   } catch (error) {
     console.error("Error al hacer feedback:", error);
