75efcc7611255b9a031b5abb96c8ce388c087291
carocor <126020764+carocor1@users.noreply.github.com>
2025-12-02 17:33:57 -0300

    feature(pantalla-reclamos-asignados): Creando pantalla de reclamos asignados.

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,64 @@
+import {
+  AtSignIcon,
+  Calendar,
+  ChartArea,
+  ChartBar,
+  Home,
+  Inbox,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Mi Subárea",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Mis Reclamos Asignados",
+    url: "#",
+    icon: AtSignIcon,
+  },
+  {
+    title: "Estadísticas",
+    url: "#",
+    icon: ChartArea,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Gestión de Reclamos</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
